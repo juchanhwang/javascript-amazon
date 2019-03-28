916757b8ae3d40b444c953084650df755afeeaa8
juchanhwang <ghkdwncks96@gmail.com>
2019-03-28 12:39:28 +0900

    add: view.js/addSuggestion, delaySuggestion 추가

--- searchUI_script/Controller/controller.js
+++ searchUI_script/Controller/controller.js
@@ -9,19 +9,17 @@ export default class Controller {
   addEvent() {
     this.optionObj.search.addEventListener('input', this.getFetchData.bind(this));
     this.autocomplete = document.querySelector('.search-autocomplete');
-    this.optionObj.search.addEventListener('blur', this.view.removeSuggestion.bind(this, this.autocomplete, this.view.minTime));
+    this.optionObj.search.addEventListener('blur', this.view.removeSuggestion.bind(this, this.autocomplete, this.view.delaySec));
   }
 
   getFetchData(event) {
     const inputVal = event.target.value;
     const responseUrl = `https://completion.amazon.com/api/2017/suggestions?session-id=146-2216035-3218645&customer-id=&request-id=DV2W3G68C9YMG9FR19CF&page-type=PrimeLandingPageHorizonte&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=73&prefix=${inputVal}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD&_=1551712792496`;
     this.model.fetchData(responseUrl)
-      .then(suggestionArr => {
-        this.showSuggestionVal(suggestionArr);
-      });
+      .then(suggestionArr => this.showSuggestionVal(suggestionArr));
   }
 
   showSuggestionVal(suggestionArr) {
-    this.view.isSuggestionVal(suggestionArr)
+    this.view.isSuggestionVal(suggestionArr);
   }
 }
--- searchUI_script/View/View.js
+++ searchUI_script/View/View.js
@@ -3,22 +3,26 @@ export default class View {
     this.autocompleteUl = document.querySelector('.autocomplete-ul');
     this.autocomplete = document.querySelector('.search-autocomplete');
     this.showSuggestion = '';
-    this.minTime = 100;
+    this.delaySec = 300;
   }
 
   isSuggestionVal(suggestionValArr) {
     this.showSuggestion = '';
     const noSuggestion = 0;
-    (suggestionValArr.length === noSuggestion) ? this.removeSuggestion(this.autocomplete, this.minTime) : this.renderSuggestion(suggestionValArr)
+    (suggestionValArr.length === noSuggestion) ? this.removeSuggestion(this.autocomplete, this.delaySec) : this.renderSuggestion(suggestionValArr)
   }
 
-  removeSuggestion(autocomplete, minTime) {
-    setTimeout(() => {
-      autocomplete.classList.remove('search-autocomplete-show');
-    }, minTime);
+  removeSuggestion(autocomplete, delaySec) {
+    setTimeout(() => autocomplete.classList.remove('search-autocomplete-show'), delaySec);
   }
 
   renderSuggestion(suggestionArr) {
+    this.addSuggestion(suggestionArr);
+    this.autocompleteUl.innerHTML = this.showSuggestion;
+    this.delayRender(this.delaySec);
+  }
+
+  addSuggestion(suggestionArr) {
     suggestionArr.forEach(suggestion => {
       let keyword = suggestion.value.split(' ').join('+');
       this.showSuggestion += `
@@ -28,8 +32,9 @@ export default class View {
         </a>
       </li>`
     });
+  }
 
-    this.autocompleteUl.innerHTML = this.showSuggestion;
-    this.autocomplete.classList.add('search-autocomplete-show');
+  delayRender(delaySec) {
+    setTimeout(() => this.autocomplete.classList.add('search-autocomplete-show'), delaySec);
   }
 }
